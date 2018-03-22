78ecd23c69c8384c7b0e4f3d4192da967bf7ed6a
Onur Ayyıldız <onur.ayyildiz@pro-line.com.tr>
2018-03-22 21:37:07 +0300

    no message

--- run.lua
+++ run.lua
@@ -77,53 +77,41 @@ model = nn.Sequential()
 
 print '==> construct model'
 
-if opt.model == 'mlp' then
-   -- Simple 2-layer neural network, with tanh hidden units
-   model:add(nn.Reshape(ninputs))
-   model:add(nn.Linear(ninputs,nhiddens))
-   model:add(nn.Tanh())
-   model:add(nn.Linear(nhiddens,noutputs))
-
-elseif opt.model == 'convnet' then
-
    if opt.type == 'cuda' then
       -- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
       model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
-      model:add(nn.Tanh())
+      model:add(nn.ReLU())
       model:add(nn.SpatialMaxPooling(poolsize,poolsize,poolsize,poolsize))
 
       -- stage 2 : filter bank -> squashing -> L2 pooling -> normalization
       model:add(nn.SpatialConvolutionMM(nstates[1], nstates[2], filtsize, filtsize))
-      model:add(nn.Tanh())
+      model:add(nn.ReLU())
       model:add(nn.SpatialMaxPooling(poolsize,poolsize,poolsize,poolsize))
 
       -- stage 3 : standard 2-layer neural network
       model:add(nn.View(nstates[2]*filtsize*filtsize))
       model:add(nn.Linear(nstates[2]*filtsize*filtsize, nstates[3]))
-      model:add(nn.Tanh())
+      model:add(nn.ReLU())
       model:add(nn.Linear(nstates[3], noutputs))
 
    else
       -- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
       model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
-      model:add(nn.Tanh())
+      model:add(nn.ReLU())
       model:add(nn.SpatialLPPooling(nstates[1],2,poolsize,poolsize,poolsize,poolsize))
       model:add(nn.SpatialSubtractiveNormalization(nstates[1], normkernel))
 
       -- stage 2 : filter bank -> squashing -> L2 pooling -> normalization
       model:add(nn.SpatialConvolutionMM(nstates[1], nstates[2], filtsize, filtsize))
-      model:add(nn.Tanh())
+      model:add(nn.ReLU())
       model:add(nn.SpatialLPPooling(nstates[2],2,poolsize,poolsize,poolsize,poolsize))
       model:add(nn.SpatialSubtractiveNormalization(nstates[2], normkernel))
 
       -- stage 3 : standard 2-layer neural network
       model:add(nn.Reshape(nstates[2]*filtsize*filtsize))
       model:add(nn.Linear(nstates[2]*filtsize*filtsize, nstates[3]))
-      model:add(nn.Tanh())
+      model:add(nn.ReLU())
       model:add(nn.Linear(nstates[3], noutputs))
-   end
-   else
-      error('unknown -model type')
    end
 
 print '==> here is the model:'
@@ -149,21 +137,6 @@ if model then
 end
 print '==> configuring optimizer'
 
-if opt.optimization == 'CG' then
-   optimState = {
-      maxIter = opt.maxIter
-   }
-   optimMethod = optim.cg
-
-elseif opt.optimization == 'LBFGS' then
-   optimState = {
-      learningRate = opt.learningRate,
-      maxIter = opt.maxIter,
-      nCorrection = 10
-   }
-   optimMethod = optim.lbfgs
-
-elseif opt.optimization == 'SGD' then
    optimState = {
       learningRate = opt.learningRate,
       weightDecay = opt.weightDecay,
@@ -171,17 +144,6 @@ elseif opt.optimization == 'SGD' then
       learningRateDecay = 1e-7
    }
    optimMethod = optim.sgd
-
-elseif opt.optimization == 'ASGD' then
-   optimState = {
-      eta0 = opt.learningRate,
-      t0 = trsize * opt.t0
-   }
-   optimMethod = optim.asgd
-
-else
-   error('unknown optimization method')
-end
 
 print '==> defining training procedure'
 -- training function
