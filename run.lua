01aa359ff62194b4ad605de55e7e29b59c520f13
Onur Ayyıldız <onur.ayyildiz@pro-line.com.tr>
2018-03-22 07:07:19 +0300

    no message

--- run.lua
+++ run.lua
@@ -19,9 +19,12 @@ cmd:option('-seed', 1, 'fixed input seed for repeatable experiments')
 cmd:option('-threads', 2, 'number of threads')
 -- data:
 cmd:option('-size', 'full', 'how many samples do we load: small | full')
+-- model:
+cmd:option('-model', 'convnet', 'type of model to construct: mlp | convnet')
 -- training:
 cmd:option('-save', 'results', 'subdirectory to save/log experiments in')
 cmd:option('-plot', false, 'live plot')
+cmd:option('-optimization', 'SGD', 'optimization method: SGD | ASGD | CG | LBFGS')
 cmd:option('-learningRate', 1e-3, 'learning rate at t=0')
 cmd:option('-batchSize', 1, 'mini-batch size (1 = pure stochastic)')
 cmd:option('-weightDecay', 0, 'weight decay (SGD only)')
@@ -73,6 +76,17 @@ classes = {'1','2','3','4','5','6','7','8','9','0'}
 model = nn.Sequential()
 
 print '==> construct model'
+
+if opt.model == 'mlp' then
+   -- Simple 2-layer neural network, with tanh hidden units
+   model:add(nn.Reshape(ninputs))
+   model:add(nn.Linear(ninputs,nhiddens))
+   model:add(nn.Tanh())
+   model:add(nn.Linear(nhiddens,noutputs))
+
+elseif opt.model == 'convnet' then
+
+   if opt.type == 'cuda' then
       -- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
       model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
       model:add(nn.Tanh())
@@ -88,6 +102,30 @@ print '==> construct model'
       model:add(nn.Linear(nstates[2]*filtsize*filtsize, nstates[3]))
       model:add(nn.Tanh())
       model:add(nn.Linear(nstates[3], noutputs))
+
+   else
+      -- stage 1 : filter bank -> squashing -> L2 pooling -> normalization
+      model:add(nn.SpatialConvolutionMM(nfeats, nstates[1], filtsize, filtsize))
+      model:add(nn.Tanh())
+      model:add(nn.SpatialLPPooling(nstates[1],2,poolsize,poolsize,poolsize,poolsize))
+      model:add(nn.SpatialSubtractiveNormalization(nstates[1], normkernel))
+
+      -- stage 2 : filter bank -> squashing -> L2 pooling -> normalization
+      model:add(nn.SpatialConvolutionMM(nstates[1], nstates[2], filtsize, filtsize))
+      model:add(nn.Tanh())
+      model:add(nn.SpatialLPPooling(nstates[2],2,poolsize,poolsize,poolsize,poolsize))
+      model:add(nn.SpatialSubtractiveNormalization(nstates[2], normkernel))
+
+      -- stage 3 : standard 2-layer neural network
+      model:add(nn.Reshape(nstates[2]*filtsize*filtsize))
+      model:add(nn.Linear(nstates[2]*filtsize*filtsize, nstates[3]))
+      model:add(nn.Tanh())
+      model:add(nn.Linear(nstates[3], noutputs))
+   end
+   else
+      error('unknown -model type')
+   end
+
 print '==> here is the model:'
 print(model)
 
@@ -98,8 +136,11 @@ print '==> define loss'
 print '==> here is the loss function:'
 print(criterion)
 ----------------------------------------------------------------------
+-- CUDA?
+if opt.type == 'cuda' then
    model:cuda()
    criterion:cuda()
+end
 -- this matrix records the current confusion across classes
 confusion = optim.ConfusionMatrix(classes)
 
@@ -107,14 +148,40 @@ if model then
    parameters,gradParameters = model:getParameters()
 end
 print '==> configuring optimizer'
-  
-  optimState = {
+
+if opt.optimization == 'CG' then
+   optimState = {
+      maxIter = opt.maxIter
+   }
+   optimMethod = optim.cg
+
+elseif opt.optimization == 'LBFGS' then
+   optimState = {
+      learningRate = opt.learningRate,
+      maxIter = opt.maxIter,
+      nCorrection = 10
+   }
+   optimMethod = optim.lbfgs
+
+elseif opt.optimization == 'SGD' then
+   optimState = {
       learningRate = opt.learningRate,
       weightDecay = opt.weightDecay,
       momentum = opt.momentum,
       learningRateDecay = 1e-7
    }
    optimMethod = optim.sgd
+
+elseif opt.optimization == 'ASGD' then
+   optimState = {
+      eta0 = opt.learningRate,
+      t0 = trsize * opt.t0
+   }
+   optimMethod = optim.asgd
+
+else
+   error('unknown optimization method')
+end
 
 print '==> defining training procedure'
 -- training function
@@ -154,6 +221,7 @@ function train()
 
       -- create closure to evaluate f(X) and df/dX
       local feval = function(x)
+	  collectgarbage()
                        -- get new parameters
                        if x ~= parameters then
                           parameters:copy(x)
@@ -187,8 +255,13 @@ function train()
                        -- return f and df/dX
                        return f,gradParameters
                     end
+
       -- optimize on current mini-batch
+      if optimMethod == optim.asgd then
+         _,_,average = optimMethod(feval, parameters, optimState)
+      else
          optim.sgd(feval, parameters, optimState)
+      end
    end
 
    -- time taken
@@ -240,7 +313,8 @@ function test(dataset)
 
       -- get new sample
       local input = testData.data[t]
-      input = input:cuda()
+      if opt.type == 'double' then input = input:double()
+      elseif opt.type == 'cuda' then input = input:cuda() end
       local target = testData.labels[t]
 
       -- test sample
